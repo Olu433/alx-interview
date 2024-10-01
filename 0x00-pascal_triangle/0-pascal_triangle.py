6c3d136cffa42d74a346458a9e5c1ad91193a134
Caleb Awe <calebawe0@gmail.com>
2024-10-01 16:44:59 +0100

    Returning a complete representation of the triangle as a list of lists.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,22 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
 def pascal_triangle(n):
     '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.
     '''
-    if not isinstance(n, int) or n <= 0:
-        return []
-
     triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
     
     for i in range(n):
-        row = [1] * (i + 1)  # Initialize row with 1s
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]  # Calculate inner values
-        triangle.append(row)
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
     
     return triangle
